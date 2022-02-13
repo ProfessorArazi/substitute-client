1faf94af74677bf439dde7a48ae3417273cfd859
Amit <amitarazi12345@gmail.com>
2022-02-13 14:10:53 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  useEffect(() => {
+    const data = {
+      userId: "61fac23a87c445711250f2b6",
+      id: "61fac28387c445711250f2bd",
+      changes: {
+        subject: "sport",
+        school: "morde",
+      },
+    };
+    axios
+      .delete(
+        process.env.REACT_APP_SERVER +
+          "/substitutes/works/61fac23a87c445711250f2b6/61fac28387c445711250f2bd"
+      )
+      .then((res) => console.log(res));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
